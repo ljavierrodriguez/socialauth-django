f1305dfea72c7d3f30109927d7e8c16277208108
Luis Rodríguez <luis.rodriguez@ORIONIS.LAN>
2018-10-23 14:50:46 -0400

    Fix Login Social Networks

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
+    path('', views.home, name="account.home"),
     path('home/', views.home, name="account.home"),
     path('settings/', views.settings, name="account.settings"),
     path('settings/password', views.password, name="account.password"),
