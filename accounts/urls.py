7c54e751dfbfbb7f1919b9724e371d64b6fb84f7
Luis Rodríguez <luis.rodriguez@ORIONIS.LAN>
2018-10-23 12:19:22 -0400

    Setting Social Account

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path, include
+from django.views.generic import TemplateView
+from . import views
 
 urlpatterns = [
-
+    path('home/', views.home, name="account.home"),
+    path('settings/', views.settings, name="account.settings"),
+    path('settings/password', views.password, name="account.password"),
 ]
